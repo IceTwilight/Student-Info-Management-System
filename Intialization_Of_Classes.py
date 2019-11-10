855af5954c406149fd52d976c8e2c665bfeb02a8
IceTwilight <assassinmelody950124@gmail.com>
2019-11-10 12:15:14 -0500

    +major +judge in student
    get student - changed, Major class: required, electives

--- Intialization_Of_Classes.py
+++ Intialization_Of_Classes.py
@@ -9,41 +9,57 @@ class Repository:
         self.info_dir = info_dir # string - directory with students, instructors, grades files
         self._students = collections.defaultdict() # key: cwid, value: instances of Student
         self._instructors = collections.defaultdict() # key: cwid, value: instances of Instructor
+        self._majors = collections.defaultdict() # key:cwid, value:instance of Major
 
         try:
+            self._get_majors(os.path.join(info_dir, 'majors.txt'))
             self._get_students(os.path.join(info_dir, 'students.txt'))
             self._get_instructors(os.path.join(info_dir, 'instructors.txt'))
-            self._get_grades(os.path.join(info_dir, 'grades.txt'))
+            self._get_grades(os.path.join(info_dir, 'grades.txt'))        
         except ValueError as ve:
             print(ve)
         except FileNotFoundError as fnfe:
             print(fnfe)
 
         if ptables:
+            print("\nMajor Summary")
+            self.majors_table()
             print("\nStudents Summary")
             self.student_table()
             print("\nInstructor Summary")
             self.instructor_table()
         
+    def _get_majors(self, path):
+        """ Read majors files and assign the course to the majors 
+            Handle exceptions in the calling function.
+        """
+        for major, flag, course in rf.file_reading_gen(path, 3, sep='\t', header=True):
+            if major not in self._majors:
+                self._majors[major] = Major(major)
+            self._majors[major].add_course(course, flag)
+
     def _get_students(self, path):
         ''' Read students from path and add to the self.students
             Allow exceptions from reading the file to flow back to the caller.
         '''
-        for cwid, name, major in rf.file_reading_gen(path, 3, sep='\t', header=False):
-            self._students[cwid] = Student(cwid, name, major)
+        for cwid, name, major in rf.file_reading_gen(path, 3, sep=';', header=True):
+            if major not in self._majors:
+                print(f"Student {cwid} '{name}' has unknown major '{major}'")
+            else:
+                self._students[cwid] = Student(cwid, name, self._majors[major])
     
     def _get_instructors(self, path):
         ''' Read instructors from path and add to the self.instructors
             Allow exceptions from reading the file to flow back to the caller.
         '''
-        for cwid, name, dept in rf.file_reading_gen(path, 3, sep='\t', header=False):
+        for cwid, name, dept in rf.file_reading_gen(path, 3, sep='|', header=True):
             self._instructors[cwid] = Instructor(cwid, name, dept)
 
     def _get_grades(self, path):
         ''' Read grades from path and add to the self.instructors
             Allow exceptions from reading the file to flow back to the caller.
         '''
-        for student_cwid, course, grade, instructor_cwid in rf.file_reading_gen(path, 4, sep='\t', header=False):
+        for student_cwid, course, grade, instructor_cwid in rf.file_reading_gen(path, 4, sep='|', header=True):
             if student_cwid in self._students:
                 self._students[student_cwid].add_course(course, grade)
             else:
@@ -53,6 +69,14 @@ class Repository:
             else:
                 print(f"Found grade for unknown instructor '{instructor_cwid}")
     
+    def majors_table(self):
+        ''' print a PrettyTable with a summary of all majors '''
+        pt = PrettyTable(field_names=Major.PT_HDR)
+        for major in self._majors.values():
+            pt.add_row(major.pt_row())
+        print(pt)
+    
+
     def student_table(self):
         ''' print a PrettyTable with a summary of all students '''
         pt = PrettyTable(field_names=Student.pt_hdr)
@@ -109,6 +133,49 @@ class Instructor:
         """
         for course, count in self._courses.items():
             yield [self._cwid, self._name, self._dept, course, count]
+
+class Major:
+    """ Track the required courses and electives for each major.
+        Provide a mechanism to store new courses and electives.
+        Provide a mechanism to identify the remaining required and elective courses given a set of passed course.
+    """
+    PT_HDR = ['Major', 'Required Courses', 'Electives']
+    PASSING_GRADES = {'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C'}
+    
+    def __init__(self, dept):
+        self._dept = dept
+        self._required = set() #set of required courses for the major - All must be taken
+        self._electives = set() #set of electives - only one must be taken
+    
+    def add_course(self, course, type):
+        """ add a new course to the major where type is either
+            'R' for required or 'E' for elective
+        """
+        if type == 'R':
+            self._required.add(course)
+        elif type == 'E':
+            self._electives.add(course)
+        else:
+            raise ValueError(f"Major.add_course: expected 'R' or 'E' but found '{type}' ")
+    
+    def remaining(self, completed):
+        """ Given a list of completed courses, calculate the reamaining required and electived courses.
+            Completed is a dict where the key is the courses completed and the grades are in PASSING_GRADES.
+            Return a tuple of (passed, rem_required, rem_electives)
+        """
+        passed = {course for course, grade in completed.items() if grade in Major.PASSING_GRADES}
+        rem_required = self._required - passed
+
+        if self._electives.intersection(passed):
+            rem_electives = None
+        else:
+            rem_electives = self._electives
+        return self._dept, passed, rem_required, rem_electives
+    
+    def pt_row(self):
+        """ Return a list of values to include in the Majors prettytable """
+        return [self._dept, sorted(self._required), sorted(self._electives)]
+
 
 def main():
     directory = '/Users/hangboli/Documents/GitHub/Student-Info-Management-System/'
